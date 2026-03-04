a88561f8ed1dfd82c122c9ccec78b0ef1c855171
Jeff <125052610+jeff203-del@users.noreply.github.com>
2026-03-04 19:46:54 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+
+  // carousel script
+  var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    loop: true
+  });
